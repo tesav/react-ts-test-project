09a625d09029b04858878160720d3c20366e4efd
Programmer <Programmer>
2021-02-25 00:37:02 +0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import FormCalculator from './FormCalculator';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app-header">
+        <h3>Calculator</h3>
       </header>
+
+      <div className="app-content">
+        <div className="form-wrap">
+          <FormCalculator />
+        </div>
+      </div>
     </div>
   );
 }
